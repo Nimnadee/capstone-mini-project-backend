ca2fe2a771af7aa02c779bb6f226a0664b950601
Dinuja Dulmina <ug.dinuja@gmail.com>
2024-04-27 10:33:09 +0530

    changed database config

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,11 +16,8 @@ import { ProjectMapper } from "./mapper/project.mapper";
 @Module({
 	imports: [
 		ConfigModule.forRoot(),
-		MongooseModule.forRoot(process.env.MONGO_HOST, {
-			dbName: process.env.MONGO_DATABASE_NAME,
-			user: process.env.MONGO_USERNAME,
-			pass: process.env.MONGO_PASSWORD,
-			authSource: process.env.MONGO_AUTH_SOURCE
+		MongooseModule.forRoot(process.env.MONGO_HOST,{
+			dbName: process.env.MONGO_DATABASE_NAME
 		}),
 		MongooseModule.forFeature([
 			{name: Student.name, schema: StudentSchema},

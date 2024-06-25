3339d601da57ec2514d5c2cbf6c66ac80dc89920
UGDDulmina <ug.dinuja@gmail.com>
2024-06-25 07:04:52 +0530

    Update feedback system

--- src/model/schema/feedback.ts
+++ src/model/schema/feedback.ts
@@ -1,13 +1,7 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 import { SchemaTypes, Types } from "mongoose";
 import { Student } from "./student";
-<<<<<<< HEAD
-
-
-
-=======
 import { Guide } from "./guide";
->>>>>>> 01390587be788f673d77e2d7d65b62f5d4e7afdb
 @Schema({ collection: "feedback" })
 export class Feedback {
 
@@ -22,17 +16,11 @@ export class Feedback {
     @Prop({ default: Date.now }) 
     public createdAt: Date;
  
-<<<<<<< HEAD
 	@Prop({ required: true, min: 1, max: 5 })
      public rating: number;
 
-=======
-	@Prop({ required: true })
-    public rating: number;
-
     @Prop({ required: true , type: SchemaTypes.ObjectId, ref: 'Guide'})
     public guide: Guide;
->>>>>>> 01390587be788f673d77e2d7d65b62f5d4e7afdb
 }
 
 export const FeedbackSchema = SchemaFactory.createForClass(Feedback);

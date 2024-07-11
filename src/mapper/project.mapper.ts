ed286a08d01a5f11a0cb715966947bd2619ca7c0
Nimnadee <snr.nimnadi@gmail.com>
2024-07-12 00:47:18 +0530

    added categories to project

--- src/mapper/project.mapper.ts
+++ src/mapper/project.mapper.ts
@@ -1,13 +1,13 @@
-import { Project } from "../model/schema/project";
-import { ProjectResponseDto } from "../model/dto/response/project.dto";
-import { StudentMapper } from "./student.mapper";
-import { ProjectRequestDto } from "../model/dto/request/project.dto";
-import { StudentRepository } from "../repository/student.repository";
-import { Injectable } from "@nestjs/common";
+import {Project} from "../model/schema/project";
+import {ProjectResponseDto} from "../model/dto/response/project.dto";
+import {StudentMapper} from "./student.mapper";
+import {ProjectRequestDto} from "../model/dto/request/project.dto";
+import {StudentRepository} from "../repository/student.repository";
+import {Injectable} from "@nestjs/common";
 import {TechnologyRepository} from "../repository/technology.repository";
 import {TechnologyMapper} from "./technology.mapper";
-import { CategoryMapper } from "./category.mapper";
-import { CategoryRepository } from "src/repository/category.repository";
+import {CategoryMapper} from "./category.mapper";
+import {CategoryRepository} from "src/repository/category.repository";
 
 @Injectable()
 export class ProjectMapper {
@@ -22,7 +22,7 @@ export class ProjectMapper {
 		projectResponseDto.title =  project.title;
 		projectResponseDto.summary =  project.summary;
 		projectResponseDto.student =  StudentMapper.studentToStudentResponseDto(await this.studentRepository.find(project.student));
-		projectResponseDto.category = CategoryMapper.categoryToCategoryResponseDto(await this.categoryRepository.find(project.category));
+		 projectResponseDto.category = project.categories.map(cat=> CategoryMapper.categoryToCategoryResponseDto(cat));
 		projectResponseDto.technologies = project.technologies.map(tech => TechnologyMapper.technologyToTechnologyResponseDto(tech));
 
 		return projectResponseDto;
@@ -33,11 +33,20 @@ export class ProjectMapper {
 		project.title = projectRequestDto.title;
 		project.summary = projectRequestDto.summary;
 		project.student = await this.studentRepository.findById(projectRequestDto.student);
-		project.category = await this.categoryRepository.findById(projectRequestDto.category);
+
+		if (projectRequestDto.category && Array.isArray(projectRequestDto.category)){
+			project.categories = await Promise.all(projectRequestDto.category.map(async (id) => {
+				return await this.categoryRepository.findById(id);
+			}));
+		} else {
+			// Handle the case where technology is not an array or is undefined
+			project.categories= []; // Or handle it as appropriate for your application
+			console.warn('Technology data is invalid or not provided');
+		}
+
 		if (projectRequestDto.technology && Array.isArray(projectRequestDto.technology)) {
 			project.technologies = await Promise.all(projectRequestDto.technology.map(async (id) => {
-			  const technology = await this.technologyRepository.findById(id);
-			  return technology;
+				return await this.technologyRepository.findById(id);
 			}));
 		  } else {
 			// Handle the case where technology is not an array or is undefined
--- src/model/dto/response/category.dto.ts
+++ src/model/dto/response/category.dto.ts
@@ -1,4 +1,4 @@
-import { Category } from "src/model/schema/category";
+
 
 export class CategoryResponseDto {
 
